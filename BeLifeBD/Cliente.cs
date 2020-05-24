ddc09aaa42447f5ae3c3267b7107ecf1cc1f5f4d
SetHet <sethet1@gmail.com>
2020-05-24 01:29:38 -0400

    Clientes Done, le falta revision

--- BeLifeBD/Cliente.cs
+++ BeLifeBD/Cliente.cs
@@ -44,13 +44,79 @@ namespace BeLifeBD
             else return null;
         }
         //Static Find all
+        public static List<Cliente> FindAll()
+        {
+            List<Cliente> list = new List<Cliente>();
+            object[][] matriz = Conexion.Select(table).ToArray();
+            Cliente c;
+            foreach (object[] row in matriz)
+            {
+                c = new Cliente();
+                c.rut = (string)row[0];
+                c.nombre = (string)row[1];
+                c.apellido = (string)row[2];
+                c.fechaNacimiento = (string)row[3];
+                c.idSexo = (int)row[4];
+                c.idEstadoCivil = (int)row[5];
+                list.Add(c);
+            }
+            return list;
+        }
 
-        //Select (id, rut)
+        //Select
+        public void Select(string rut = null)
+        {
+            if (rut == null) rut = this.rut;
+            rut = Tools.StringLength(rut, rut_length);
+            object[] row = Conexion.SelectFirst(table, where: $"rutCliente = '{rut}'");
+            if (row != null)
+            {
+                this.rut = (string)row[0];
+                this.nombre = (string)row[1];
+                this.apellido = (string)row[2];
+                this.fechaNacimiento = (string)row[3];
+                this.idSexo = (int)row[4];
+                this.idEstadoCivil = (int)row[5];
+            }
+            else
+            {
+                Console.WriteLine("No se encontro cliente, rut: " + rut);
+            }
+        }
 
         //Delete
+        public bool Delete(string rut = null)
+        {
+            if (rut == null) rut = this.rut;
+            rut = Tools.StringLength(rut, rut_length);
+            int? num = Conexion.Delete(table, where: $"rutCliente = '{rut}'");
+            if (num == null || num < 1)
+            {
+                return false;
+            }
+            else return true;
+        }
 
         //Update
+        public bool Update()
+        {
+            if (!Exist()) return false;
+            string rut = Tools.StringLength(this.rut, rut_length);
+            int? i = Conexion.Update(table, 
+                $"nombres = '{this.nombre}', apellidos = '{this.apellido}', fechaNacimiento = '{this.fechaNacimiento}', idSexo = {this.idSexo}, idEstadoCivil = {this.idEstadoCivil}", 
+                $"rutCliente = '{rut}'");
+            if (i == null || i < 1) return false;
+            else return true;
+        }
 
+
+        //Exist
+        public bool Exist(string rut = null)
+        {
+            if (rut == null) rut = this.rut;
+            rut = Tools.StringLength(rut, rut_length);
+            return Conexion.SelectValue(table, "rutCliente", $"rutCliente = '{rut}'") != null;
+        }
 
 
 
